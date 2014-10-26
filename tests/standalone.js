c5b57817d8298f75ea240c456c1dbcf262452890
Pedro Teixeira <i@pgte.me>
2014-10-26 14:45:15 +0000

    standalone tests

--- tests/standalone.js
+++ tests/standalone.js
@@ -6,14 +6,28 @@ var it = lab.it;
 var assert = Lab.assert;
 var describe = lab.describe;
 
+var path = require('path');
+var async = require('async');
+var concat = require('concat-stream');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+
 var Node = require('../');
 
 describe('standalone', function() {
 
+  var dbPath = path.join(__dirname, '..', 'db');
+
+  rimraf.sync(dbPath);
+  mkdirp.sync(dbPath);
+
   var node;
+  var commands;
 
   it('can get created', function(done) {
-    node = Node('tcp+msgpack://localhost:8080');
+    node = Node('tcp+msgpack://localhost:8080', {
+      dbPath: dbPath
+    });
     done();
   });
 
@@ -46,5 +60,40 @@ describe('standalone', function() {
       assert.equal(err.message, 'Key not found in database');
       done();
     });
+  });
+
+  it('can delete', function(done) {
+    node.del('key', done);
+  });
+
+  it('delete worked', function(done) {
+    node.get('key', function(err) {
+      assert.instanceOf(err, Error);
+      assert.equal(err.message, 'Key not found in database');
+      done();
+    });
+  });
+
+  it('can create a write stream', function(done) {
+    commands = [];
+    for (var i = 0 ; i < 10 ; i ++) {
+      commands.push({
+        type: 'put',
+        key: 'key ' + i,
+        value: 'value ' + i
+      });
+    }
+    var ws = node.createWriteStream();
+    async.each(commands, ws.write.bind(ws), done);
+  });
+
+  it('can create a read stream', function(done) {
+    commands.forEach(function(command) {
+      delete command.type;
+    });
+
+    node.createReadStream().pipe(concat(function(data) {
+      assert.deepEqual(data, commands);
+    })).once('finish', done);
   });
 });
